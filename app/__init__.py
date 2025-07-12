bf94d7b6b9fbc4a6745725cfc1a181844de48bb5
KyoDev-debug <Sugaruchan2434@gmail.com>
2025-07-13 00:01:49 +0900

    コミットバグ修正

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("config.py")  # ← 次の段階でDB設定などを読み込み
 
     from app.routes import main
     app.register_blueprint(main)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return "Hello, Task Manager!"
+    return render_template("index.html")
