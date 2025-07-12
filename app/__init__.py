546262ba7464e5fec0460f3fb98701c2ba7b1564
KyoDev-debug <Sugaruchan2434@gmail.com>
2025-07-13 00:24:30 +0900

    config作成

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
 import os
+from flask import Flask
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
     app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "..", "templates"))
-    app.config.from_pyfile("config.py")  # ← 後ほどDB設定などを追加
+    app.config.from_pyfile("config.py")
 
     from app.routes import main
     app.register_blueprint(main)
