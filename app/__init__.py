d0a668d97be95d88d2974d2157f3fc3897bf1e32
KyoDev-debug <Sugaruchan2434@gmail.com>
2025-07-13 00:11:16 +0900

    htmlファイルが見つからないバグ修正

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile("config.py")  # ← 次の段階でDB設定などを読み込み
+    app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "..", "templates"))
+    app.config.from_pyfile("config.py")  # ← 後ほどDB設定などを追加
 
     from app.routes import main
     app.register_blueprint(main)
